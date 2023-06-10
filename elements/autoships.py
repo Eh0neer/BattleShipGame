2a80bb65b3d5eabc058d75cc1e0a42d8badbbda0
smliity <sml1ty@ya.ru>
2023-06-10 21:58:46 +0700

    finaly

--- elements/autoships.py
+++ elements/autoships.py
@@ -1,61 +1,61 @@
-"""Creates human ships automatically."""
+"""Автоматически создает человеческие корабли."""
 
 from random import choice, randint
 
 
 class AutoShips:
     """
-    Randomly create all player's ships on a grid
+    Случайное создание всех кораблей игрока на сетке
     ----------
-    Attributes:
-        offset (int): Where the grid starts (in number of blocks)
-                (typically 0 for computer and 15 for human)
-        available_blocks (set of tuples): coordinates of all blocks
-                that are available for creating ships (updated every time a ship is created)
-        ships_set (set of tuples): all blocks that are occupied by ships
-        ships (list of lists): list of all individual ships (as lists)
+    Атрибуты:
+        offset (int): Место начала сетки (в количестве блоков).
+                (обычно 0 для компьютера и 15 для человека)
+        available_blocks (набор кортежей): координаты всех блоков
+                которые доступны для создания кораблей (обновляются каждый раз, когда создается корабль)
+        ships_set (множество кортежей): все блоки, которые заняты кораблями
+        ships (список списков): список всех отдельных кораблей (в виде списков)
     ----------
-    Methods:
+    Методы:
         __create_start_block(available_blocks):
-            Randomly chooses a block from which to start creating a ship.
-            Randomly chooses horizontal or vertical type of a ship
-            Randomly chooses direction (from the start block) - straight or reverse
-            Returns three randomly chosen values
+            Случайным образом выбирает блок, с которого начнется создание корабля.
+            Случайно выбирает горизонтальный или вертикальный тип корабля.
+            Случайным образом выбирает направление (от стартового блока) - прямое или обратное.
+            Возвращает три случайно выбранных значения
         __create_ship(number_of_blocks, available_blocks):
-            Creates a ship of given length (number_of_blocks) starting from the start block
-                returned by the previous method, using type of ship and direction (changing it
-                if going outside of grid) returned by previous method.
-                Checks if the ship is valid (not adjacent to other ships and within the grid)
-                and adds it to the list of ships.
-            Returns: a list of tuples with a new ship's coordinates
+            Создает корабль заданной длины (количество_блоков), начиная со стартового блока
+                возвращенного предыдущим методом, используя тип корабля и направление (изменяя его
+                если выходит за пределы сетки), возвращенные предыдущим методом.
+                Проверяет, является ли корабль действительным (не примыкает к другим кораблям и находится в пределах сетки)
+                и добавляет его в список кораблей.
+            Возвращает: список кортежей с координатами нового корабля.
         __get_new_block_for_ship(self, coor, direction, orientation, ship_coordinates):
-            Checks if new individual blocks that are being added to a ship in the previous method
-                are within the grid, otherwise changes the direction.
-            Returns:
-                direction (int): straight or reverse
-                incremented/decremented coordinate of the last/first block in a ship under construction
+            Проверяет, находятся ли новые отдельные блоки, добавляемые к кораблю в предыдущем методе.
+                находятся в пределах сетки, в противном случае изменяет направление.
+            Возвращает:
+                направление (int): прямое или обратное
+                увеличивает/уменьшает координаты последнего/первого блока в строящемся корабле
         __is_ship_valid(new_ship):
-            Check if all of a ship's coordinates are within the available blocks set.
-            Returns: True or False
+            Проверяет, все ли координаты корабля находятся в пределах доступного набора блоков.
+            Возвращает: True или False
         __add_new_ship_to_set(new_ship):
-            Adds all blocks in a ship's list to the ships_set
+            Добавляет все блоки из списка корабля в набор ships_set.
         __update_available_blocks_for_creating_ships(new_ship):
-            Removes all blocks occupied by a ship and around it from the available blocks set
+            Удаляет все блоки, занятые кораблем и вокруг него, из набора доступных блоков.
         __populate_grid():
-            Creates needed number of each type of ships by calling the create_ship method.
-                Adds every ship to the ships list, ships_set and updates the available blocks.
-            Returns: the list of all ships
+            Создает необходимое количество кораблей каждого типа, вызывая метод create_ship.
+                Добавляет каждый корабль в список кораблей, ships_set и обновляет доступные блоки.
+            Возвращает: список всех кораблей
     """
 
     def __init__(self, offset: int) -> None:
         """
-        Parameters:
-        offset (int): Where the grid starts (in number of blocks)
-                (typically 0 for computer and 15 for human)
-        available_blocks (set of tuples): coordinates of all blocks
-                that are avaiable for creating ships (updated every time a ship is created)
-        ships_set (set of tuples): all blocks that are occupied by ships
-        ships (list of lists): list of all individual ships (as lists)"""
+        Параметры:
+        offset (int): Где начинается сетка (количество блоков)
+                (обычно 0 для компьютера и 15 для человека)
+        available_blocks (набор кортежей): координаты всех блоков
+                доступные для создания кораблей (обновляются каждый раз при создании корабля)
+        ship_set (набор кортежей): все блоки, занятые кораблями
+        корабли (список списков): список всех отдельных кораблей (в виде списков)"""
 
         self.offset = offset
         self.available_blocks = {(x, y) for x in range(1 + self.offset, 11 + self.offset) for y in range(1, 11)}
@@ -66,17 +66,17 @@ class AutoShips:
 
     def __create_start_block(self, available_blocks: set[tuple]) -> tuple:
         """
-        Randomly chooses a block from which to start creating a ship.
-        Randomly chooses horizontal or vertical type of a ship
-        Randomly chooses direction (from the start block) - straight or reverse
-        Args:
-            available_blocks (set of tuples): coordinates of all blocks
-                that are available for creating ships (updated every time a ship is created)
-        Returns:
-            int: x coordinate of a random block
-            int: y coordinate of a random block
-            int: 0=horizontal (change x), 1=vertical (change y)
-            int: 1=straight, -1=reverse
+        Случайным образом выбирает блок, с которого начинается создание корабля.
+        Случайным образом выбирает горизонтальный или вертикальный тип корабля
+        Случайным образом выбирает направление (от начального блока) - прямо или наоборот
+        Аргументы:
+            available_blocks (набор кортежей): координаты всех блоков
+                доступные для создания кораблей (обновляются каждый раз при создании корабля)
+        Возвращает:
+            int: x координата случайного блока
+            int: координата y случайного блока
+            int: 0=горизонтально (изменить x), 1=вертикально (изменить y)
+            int: 1=прямой, -1=обратный
         """
         self.orientation = randint(0, 1)
         # -1 is left or down, 1 is right or up
@@ -86,16 +86,16 @@ class AutoShips:
 
     def __create_ship(self, number_of_blocks: int, available_blocks: set[tuple]) -> list:
         """
-        Creates a ship of given length (number_of_blocks) starting from the start block
-                returned by the previous method, using type of ship and direction (changing it
-                if going outside of grid) returned by previous method.
-                Checks if the ship is valid (not adjacent to other ships and within the grid)
-                and adds it to the list of ships.
-        Args:
-            number_of_blocks (int): length of a needed ship
-            available_blocks (set): free blocks for creating ships
-        Returns:
-            list: a list of tuples with a new ship's coordinates
+        Создает корабль заданной длины (number_of_blocks), начиная с начального блока
+                возвращается предыдущим методом, используя тип корабля и направление (изменив его
+                при выходе за пределы сетки) возвращается предыдущим методом.
+                Проверяет, действителен ли корабль (не рядом с другими кораблями и в пределах сетки)
+                и добавляет его в список кораблей.
+        Аргументы:
+            number_of_blocks (int): длина необходимого корабля
+            available_blocks (набор): свободные блоки для создания кораблей
+        Возвращает:
+            list: список кортежей с координатами нового корабля
         """
         ship_coordinates = []
         x, y, self.orientation, self.direction = self.__create_start_block(available_blocks)
@@ -111,16 +111,16 @@ class AutoShips:
 
     def __get_new_block_for_ship(self, coor: int, direction: int, orientation: int, ship_coordinates: list) -> tuple:
         """
-        Checks if new individual blocks that are being added to a ship in the previous method
-                are within the grid, otherwise changes the direction.
-        Args:
-            coor (int): x or y coordinate to increment/decrement
-            direction (int): 1 or -1
-            orientation (int): 0 or 1
-            ship_coordinates (list): coordinates of unfinished ship
-        Returns:
-            direction (int): straight or reverse
-            incremented/decremented coordinate of the last/first block in a ship under construction (int)
+        Проверяет, добавляются ли новые отдельные блоки к кораблю предыдущим методом.
+                находятся внутри сетки, в противном случае меняет направление.
+        Аргументы:
+            coor (int): координата x или y для увеличения/уменьшения
+            направление (целое): 1 или -1
+            ориентация (целое): 0 или 1
+            ship_coordinates (список): координаты недостроенного корабля
+        Возвращает:
+            направление (int): прямое или обратное
+            увеличенная/уменьшенная координата последнего/первого блока строящегося корабля (int)
         """
         self.direction = direction
         self.orientation = orientation
@@ -133,28 +133,28 @@ class AutoShips:
 
     def __is_ship_valid(self, new_ship: list) -> bool:
         """
-        Check if all of a ship's coordinates are within the available blocks set.
-        Args:
-            new_ship (list): list of tuples with a newly created ship's coordinates
-        Returns:
-            bool: True or False
+        Проверьте, все ли координаты корабля находятся в пределах набора доступных блоков.
+        Аргументы:
+            new_ship (список): список кортежей с координатами вновь созданного корабля
+        Возвращает:
+            bool: правда или ложь
         """
         ship = set(new_ship)
         return ship.issubset(self.available_blocks)
 
     def __add_new_ship_to_set(self, new_ship: list) -> None:
         """
-        Adds all blocks in a ship's list to the ships_set
-        Args:
-            new_ship (list): list of tuples with a newly created ship's coordinates
+        Добавляет все блоки в списке кораблей в ship_set
+        Аргументы:
+            new_ship (список): список кортежей с координатами вновь созданного корабля
         """
         self.ships_set.update(new_ship)
 
     def __update_available_blocks_for_creating_ships(self, new_ship: list) -> None:
         """
-        Removes all blocks occupied by a ship and around it from the available blocks set
-        Args:
-            new_ship ([type]): list of tuples with a newly created ship's coordinates
+        Удаляет все блоки, занятые кораблем и вокруг него, из набора доступных блоков
+        Аргументы:
+            new_ship ([type]): список кортежей с координатами вновь созданного корабля
         """
         for elem in new_ship:
             for k in range(-1, 2):
@@ -164,10 +164,10 @@ class AutoShips:
 
     def __populate_grid(self) -> list:
         """
-        Creates needed number of each type of ships by calling the create_ship method.
-                Adds every ship to the ships list, ships_set and updates the available blocks.
-        Returns:
-            list: the 2d list of all ships
+        Создает необходимое количество кораблей каждого типа, вызывая метод create_ship.
+                Добавляет каждый корабль в список кораблей, ship_set и обновляет доступные блоки.
+        Возвращает:
+            list: второй список всех кораблей
         """
         ships_coordinates_list = []
         for number_of_blocks in range(4, 0, -1):
--- elements/constants.py
+++ elements/constants.py
@@ -1,4 +1,4 @@
-"""Stores game constants"""
+""'Хранит игровые константы'""
 
 # Colors
 WHITE = (255, 255, 255)
@@ -23,9 +23,9 @@ LINE_WIDTH = 1
 # Button constants
 AUTO_BUTTON_PLACE = LEFT_MARGIN + 17 * BLOCK_SIZE
 MANUAL_BUTTON_PLACE = LEFT_MARGIN + 20 * BLOCK_SIZE
-HOW_TO_CREATE_SHIPS_MESSAGE = "How do you want to create your ships? Click the button"
+HOW_TO_CREATE_SHIPS_MESSAGE = "Как вы хотите создавать свои корабли? Нажмите кнопку"
 UNDO_BUTTON_PLACE = LEFT_MARGIN + 19 * BLOCK_SIZE
-PLAY_AGAIN_MESSAGE = "Do you want to play again or quit?"
+PLAY_AGAIN_MESSAGE = "Вы хотите играть снова или выйти?"
 PLAY_AGAIN_BUTTON_PLACE = LEFT_MARGIN + 15 * BLOCK_SIZE
 
 # Rectangular areas for grids, messages, buttons and ship counts
